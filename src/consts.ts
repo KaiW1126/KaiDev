830c69203fdc911d18a89406fdec784cf7216fef
Kai <oarkaiw6211@gmail.com>
2026-02-07 13:20:16 +0900

    feat: Phase 1 - Dark mode, Header/Footer improvements
    - Add dark mode support with CSS variables and ThemeToggle component
    - Update Header with sticky nav, X/GitHub social links, dark mode toggle
    - Update Footer with social links (X, GitHub, Zenn) and copyright
    - Add inline dark mode init script to prevent flash
    - Update global.css with modern color palette
    - Add SOCIAL_LINKS to consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'KaiDev';
+export const SITE_DESCRIPTION = '学習と開発の記録を発信する個人ブログ';
+
+// Social Links - あなたのアカウントURLに更新してください
+export const SOCIAL_LINKS = {
+  x: 'https://twitter.com/YOUR_TWITTER_HANDLE', // TODO: あなたのXアカウントに変更
+  github: 'https://github.com/KaiW1126', 
+  zenn: 'https://zenn.dev/kai_skrj', // TODO: あなたのZennアカウントに変更
+};
